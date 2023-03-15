f7a14222d12fa3d158c1e0cc63967ceb7bb82b02
Palina Brahina <s24601@pjwstk.edu.pl>
2023-03-15 10:41:34 +0100

    test commit

---
+++ Lab01/main.py
@@ -0,0 +1,14 @@
+"""
+Lab01 - Wprowadzenie do Python
+"""
+
+print("Hi")
+
+# Pusta linia
+print()
+
+# Podstawowe formatowanie (2.7)
+print("Mam na imie %s. Mam %d lat."%("Pola", 22))
+
+# Podstawowe formatowanie (3)
+print("Mam na imie {0}. Mam {1} lat.".format("Pola", 22))
